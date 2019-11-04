7312ffa7505e37ce68c349edb58849ee7f142fa2
wuhonglei <1017368065@qq.com>
2019-11-04 23:47:52 +0800

    save_response 增加 filetype 参数

--- webvpn/webvpn/spiders/example.py
+++ webvpn/webvpn/spiders/example.py
@@ -8,7 +8,7 @@ from webvpn.items import WebvpnItem
 
 class ExampleSpider(scrapy.Spider):
     name = 'example'
-    start_urls = ['http://www.baidu.com/']
+    start_urls = ['http://www.baidu.com']
     selector = {
         'css': 'link[rel=stylesheet][href]::attr(href)',
         'script': 'script[src]::attr(src)',
@@ -17,21 +17,21 @@ class ExampleSpider(scrapy.Spider):
 
     def parse(self, response):
         for link in response.css(self.selector.get('css')).getall():
-            yield response.follow(link, callback=self.save_response)
+            yield response.follow(link, callback=self.save_response, meta={'filetype': 'css'})
         
         for script in response.css(self.selector.get('script')).getall():
-            yield response.follow(script, callback=self.save_response)
+            yield response.follow(script, callback=self.save_response, meta={'filetype': 'javascript'})
 
-        self.save_response(response)
         for next_page in response.css(self.selector.get('a')).getall():
             if 'javascript' not in next_page:
                 yield response.follow(next_page, callback=self.parse)
 
 
     def save_response(self, response):
-        '''保存css，含有 filetype、filepath、filename、filesize和body'''
+        '''保存文件信息，含有 filetype、filepath、filename、filesize 和 body'''
         item = WebvpnItem()
-        item['filetype'] = 'css'
+
+        item['filetype'] = response.meta.get('filetype')
         item['filepath'] = response.url
         item['filename'] = self.get_filename(response.url)
         item['filesize'] = len(response.body)
@@ -42,4 +42,4 @@ class ExampleSpider(scrapy.Spider):
     def get_filename(self, url):
         '''从路径中获取文件名'''
         uri = urlparse(url)
-        return os.path.basename(uri.path)
+        return os.path.basename(uri.path or uri.query)
